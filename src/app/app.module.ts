685ad06cbc7f4d07124aba0e1694e9e8b53d3530
Mariam Fane <fanemariam162@gmail.com>
2023-02-08 22:01:10 +0000

    creation des nouvelles fichiers

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,9 +1,15 @@
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
+// pour resoudre le probleme de la langue que angular prend en compte on import registerLocaleData et locales fr
+
+import { registerLocaleData } from '@angular/common';
+import localeFr from '@angular/common/locales/fr';
 
 import { AppCompement } from './app.component';
 import { HotelListComponent } from './hotel-list/hotel-list.component';
+
+registerLocaleData(localeFr, 'fr');
 
 @NgModule({
   declarations: [ AppCompement , HotelListComponent],
--- src/app/hotel-list/hotel-list.component.ts
+++ src/app/hotel-list/hotel-list.component.ts
@@ -1,35 +1,37 @@
-import { Component } from "@angular/core";
+import { Component, OnInit } from "@angular/core";
+import { IHotel } from "./hotel";
 
 @Component({
   selector: 'app-hotel-list',
-  templateUrl: './hotel-list.component.html'
+  templateUrl: './hotel-list.component.html',
+  styleUrls: ['./hotel-list.component.css']
 })
 
-export class HotelListComponent{
+export class HotelListComponent implements OnInit {
     public title = 'Liste hotels';
     /*les directives angular ngIf et && permt d'ajoute les attributs qu'on a cree soit meme ou par angular*/
-    public hotels:any[] = [
+    public hotels: IHotel[] = [
       {
       hotelId: 1,
-      HotelName: 'Buea sweet life',
+      hotelName: 'Buea sweet life',
       description: 'Belle vue au bord de la mer',
       price: 230.5,
       imageUrl: 'assets/img/hotel-room.jpg'
     }, {
       hotelId: 2,
-      HotelName: 'Marakech',
+      hotelName: 'Marakech',
       description: 'Profitez de la vue sur les montagnes',
       price: 145.5,
       imageUrl: 'assets/img/the-interior.jpg'
     },{
       hotelId: 3,
-      HotelName: 'Buea sweet life',
+      hotelName: 'Buea sweet life',
       description: 'Belle vue au bord de la mer',
       price: 120.51,
       imageUrl: 'assets/img/indoors.jpg'
     },{
       hotelId: 4,
-      HotelName: 'Cape town city',
+      hotelName: 'Cape town city',
       description: 'Magnifique cadre pour votre sejour',
       price: 135.12,
       imageUrl: 'assets/img/window.jpg'
@@ -38,10 +40,34 @@ export class HotelListComponent{
 
   public showBadge: boolean = false;
 
-  public hotelFilter = 'mot';
+  private _hotelFilter = 'mot';
+  public filteredHotels: IHotel[] = [];
+
+  ngOnInit(){
+   this.filteredHotels = this.hotels;
+   this.hotelFilter = 'mot';
+  }
 
   public toggleIsNewBadge(): void{
       this.showBadge = !this.showBadge;
+    }
+
+    public get hotelFilter(): string{
+      return this._hotelFilter;
+    }
+
+    public set hotelFilter(filter: string){
+      this._hotelFilter = filter;
+      this.filteredHotels = this.hotelFilter ? this.filterHotels(this.hotelFilter): this.hotels;
+    }
+
+    private filterHotels(criteria: string) : IHotel[] {
+      criteria = criteria.toLocaleLowerCase();
+
+      const res = this.hotels.filter(
+        (hotel: IHotel) => hotel.hotelName.toLocaleLowerCase(criteria).indexOf(criteria) != -1
+      );
+      return res;
     }
 
 }
